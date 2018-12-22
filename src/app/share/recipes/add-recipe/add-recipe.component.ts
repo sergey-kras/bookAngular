4110764c9b1f035d5b63aa97484f638e66a292b0
SergeyKR <vip.41243@gmail.com>
2018-12-22 13:06:19 +0700

    ingr

--- src/app/share/recipes/add-recipe/add-recipe.component.ts
+++ src/app/share/recipes/add-recipe/add-recipe.component.ts
@@ -18,15 +18,31 @@ export class AddRecipeComponent implements OnInit {
     this.form = new FormGroup({
       title: new FormControl('', [Validators.required]),
       comment: new FormControl('', [Validators.required]),
+      ingred: new FormControl('', [Validators.required]),
     });
   }
   onAddRecipe() {
-    console.log(this.form.value);
+    const recipe = this.form.value;
+    recipe['ingridients'] = [];
+    const ingrobj = {};
+    const ingred = this.form.value.ingred.split('\n');
+    ingred.map(single => {
+      const singleIngr = this.makeIngred(single);
+      ingrobj[singleIngr[0]] = singleIngr[1];
+    });
+    recipe['ingridients'].push(ingrobj);
+    console.log(recipe);
     this.recipe.emit(this.form.value);
     this.http.post(`${environment.apiUrl}/recipes`, this.form.value).subscribe();
     this.form.reset();
     this.form.controls['title'].setValidators(null);
     this.form.controls['comment'].setValidators(null);
+    this.form.controls['ingred'].setValidators(null);
+  }
+
+  makeIngred(single) {
+    single = single.split(':');
+    return single;
   }
 
 }
